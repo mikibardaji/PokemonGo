1dd3a66c68f14f9e6c64e10c890ee715b3eb72ce
Miquel Angel Bardaji <mikibardaji@hotmail.com>
2024-04-26 11:27:21 +0200

    Esquelet inicial

---
+++ PokemonGo/src/fitxers/Caratula.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fitxers;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mabardaji logo.pok
+ */
+public class Caratula {
+    /*atributs necessaris per llegir un fitxer*/
+    
+    
+    /*constructor necessari*/
+    
+    
+    /*recuperarDatos*/
+    public ArrayList<String> recuperarDatos(/* */)
+    {
+        //abrir fichero
+        //recuperar linea a linea y guardarlo en un arraylist<String>
+        //retornar el arrayList<String>
+        //no sout!!!
+        return null;
+    }
+    
+}
---
+++ PokemonGo/src/pokemongo/PokemonGo.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pokemongo;
+
+/**
+ *
+ * @author mabardaji
+ */
+public class PokemonGo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PokemonGo app = new PokemonGo();
+        app.run();
+    }
+
+    /* la ejecucion programa*/
+    private void run() {
+        /*mostrar caratula*/
+            /*recuperar datos fichero de caratula*/
+            
+            /*mostrar por pantalla caratula
+            recorrer arraylistStirng y mostrarlo por pantalla*/
+            
+        /*mostrar el menu 
+            con 0 debe salir programa
+            con otras opciones mostrar una frase 
+            "has dado en la funcion XXXX")
+         */
+        
+    }
+    
+}
